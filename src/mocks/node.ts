dd5ffeabde60292411da4526d12212b7088b8274
dongkyukim1 <wlsntus55@google.com>
2025-03-28 23:54:23 +0900

    패키지 의존성 업데이트 및 코드 개선
    - @types/node 버전 업데이트: 20.11.19에서 20.17.28로 변경
    - layout.tsx에서 KakaoScript를 동적으로 로드하도록 수정
    - Redirect 컴포넌트에서 로그인 처리 로직 useEffect로 변경
    - NextAuthProvider에서 session 및 refetch 관련 설정 추가
    - MSW 2.0에 맞춰 node 서버 설정 변경

--- src/mocks/node.ts
+++ src/mocks/node.ts
@@ -1,4 +1,10 @@
-import { setupServer } from 'msw/node'
+// MSW 2.0 버전에서는 node 서버가 필요 없음
 import { handlers } from './handlers'
 
-export const server = setupServer(...handlers)
+// createServer() 대신 빈 객체를 내보냄
+export const server = {
+  listen: () => {},
+  close: () => {},
+  use: () => {},
+  resetHandlers: () => {},
+}
